e296220c6a646dbedb9252b714ddf04652ba22d3
NadAn12 <147739807+NadAn12@users.noreply.github.com>
2024-12-25 23:21:19 +0700

    разработка списка статьи

--- app/src/main/java/com/example/news/presentation/common/ArticlesList.kt
+++ app/src/main/java/com/example/news/presentation/common/ArticlesList.kt
@@ -2,30 +2,46 @@ package com.example.news.presentation.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.motion.widget.MotionScene.Transition.TransitionOnClick
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.example.news.domain.model.Article
+import com.example.news.presentation.Dimens.ExtraSmallPadding2
 import com.example.news.presentation.Dimens.MediumPadding1
-import kotlin.math.E
 
 @Composable
 fun ArticlesList(
     modifier: Modifier = Modifier,
-    article: LazyPagingItems<Article>,
+    articles: LazyPagingItems<Article>,
     onClick:(Article) -> Unit
 ){
+    val handlePagingResult = handlePagingResult(articles = articles)
 
+    if (handlePagingResult){
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+            contentPadding = PaddingValues(all = ExtraSmallPadding2)
+        ){
+            items(count = articles.itemCount){
+                articles[it]?.let{
+                    ArticleCard(article = it, onClick = { onClick(it)})
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun handlePagingResult( articles: LazyPagingItems<Article>, ):Boolean {
     val loadState = articles.loadState
     val error = when{
-        loadState is LoadState.Error -> loadState.refresh as LoadState.Error
+        loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
         loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
         loadState.append is LoadState.Error -> loadState.append as LoadState.Error
         else -> null
---
+++ app/src/main/java/com/example/news/presentation/home/HomeScreen.kt
@@ -0,0 +1,5 @@
+package com.example.news.presentation.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
